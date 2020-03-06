84c14907707e4f96d4a033cfdef4f6a42ea17de5
deepakksingh <deepak.singh@research.iiit.ac.in>
2020-03-06 19:04:40 +0530

    added initial db code

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,19 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy 
+from flask_migrate import Migrate 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+
+from app import routes, models
+
+#models: is used to define the structure of the database
+
+#migration: most of the relational databases are centered around 
+#structured data, so when the structure changes the data that
+#is already in the database, the database needs to be migrated
+#to the modified structure.
